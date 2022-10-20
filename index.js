67d5cc8ec97ddf129e73e3d92ebe74c5fd64b7ad
yousif <yousif.aziz@filnady.app>
2022-10-20 19:59:26 +0200

    promis

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-const math = require("mathjs")
-
-
-function greetings(name) {
-    if (!name)
-        throw new Error("missing name");
-
-    let messages = [
-        `welcome Mr ${name}`,
-        `Good afternoon ${name}`,
-        `Hi ${name}`,
-        `Good morning ${name}`,
-        `Have a nice day, Mr  ${name}`,
-    ]
-
-    let num = Random()
-    while (num >= messages.length)
-        num = Random()
-
-    console.log(messages[num]);
-}
-
-function Random() {
-    return Math.round(Math.random() * 10);
-}
-
-for (let i = 2; i < process.argv.length; i++)
-    greetings(process.argv[i])
-
-console.log(math.sqrt(16))
+// // let x;
+// // console.log(x)
+// // x = 3 + 2;
+// // console.log(x)
+// // x = x + 5
+// // console.log(x)
+// //
+// // setTimeout(() => {
+// //     console.log(x)
+// // }, 5000)
+// // x = 0;
+//
+// function read(filename) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("this is the content")
+//             // InFail("errrrr")
+//         }, 3000)
+//     });
+// }
+//
+// read("fil1")
+//     .then(async (r1) => ({oldcontent: r1, newconent: await read("file2")}))
+//     .then(co => console.log(co.oldcontent + co.newconent))
+//     .catch(err => {
+//         console.log("errr", err)
+//     })
+//
+//
+// //     .then(read("file2"))
+// //     .then(read("file3"))
+// //     .then(read("file4"))
+// //     // .then(read("file5"))
+// //     .then(read("file6"))
+// //
+// //
+// // read("context.txt", (content1) => {
+// //
+// //     read("context.txt", (content2) => {
+// //         read("context.txt", (content3) => {
+// //             read("context.txt", (content4) => {
+// //                 read("context.txt", (content1) => {
+// //                     // read("context.txt", (content2) => {
+// //                         read("context.txt", (content3) => {
+// //
+// //                         })
+// //                     })
+// //                 })
+// //             })
+// //         // })
+// //     })
+// // })
+// //
+// //
+// //
+// //
+//
+// read("context.txt", (content4) => {
+// })
